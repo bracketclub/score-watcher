54440d34c30dfbee745ca150ec86c8ae8ce78d56
Luke Karrys <lukekarrys@gmail.com>
2013-03-22 13:42:01 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+/*global console */
+
+var express = require('express'),
+    http = require('http'),
+    log = require('./lib/log'),
+    app = express(),
+    scores = require('./lib/scores');
+
+app.configure(function () {
+  app.set('port', 8080);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+  log.debug('Express server listening', app.get('port'), app.settings.env);
+  scores.watch();
+});
