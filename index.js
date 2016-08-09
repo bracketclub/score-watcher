c3b86f188ca2bdc8c4a87d08c6f6ed61aaaf5426
Luke Karrys <lukekarrys@gmail.com>
2016-08-08 21:14:16 -0700

    deps

--- index.js
+++ index.js
@@ -1,125 +1,121 @@
-'use strict';
+'use strict'
 
-const bucker = require('bucker');
-const _ = require('lodash');
-const Scores = require('scores');
-const BracketUpdater = require('bracket-updater');
-const bracketData = require('bracket-data');
-const async = require('async');
+const bucker = require('bucker')
+const _ = require('lodash')
+const Scores = require('scores')
+const BracketUpdater = require('bracket-updater')
+const bracketData = require('bracket-data')
+const async = require('async')
 
 class Watcher {
-  constructor(options) {
+  constructor (options) {
     this.options = _.defaults(options || {}, {
       logger: null,
       sport: 'ncaam',
       year: new Date().getFullYear(),
       master: '',
-      onSave() {},
-      onDrain() {},
+      onSave () {},
+      onDrain () {},
       scores: {}
-    });
+    })
 
-    const sport = this.options.sport;
-    const year = this.options.year;
-    const empty = bracketData({year, sport}).constants.EMPTY;
-    const master = this.options.master || empty;
+    const sport = this.options.sport
+    const year = this.options.year
+    const empty = bracketData({year, sport}).constants.EMPTY
+    const master = this.options.master || empty
 
     if (!sport || !year) {
-      throw new Error('Needs a sport and a year');
+      throw new Error('Needs a sport and a year')
     }
 
     // The logger will be passed to the score watcher to log everything there.
-    this.logger = this.options.logger || bucker.createNullLogger();
+    this.logger = this.options.logger || bucker.createNullLogger()
 
     // Create the score watcher
-    this.scores = new Scores(_.extend({logger: this.logger}, this.options.scores));
-    this.scores.on('event', (event) => this.onEvent(event));
+    this.scores = new Scores(_.extend({logger: this.logger}, this.options.scores))
+    this.scores.on('event', this.onEvent.bind(this))
 
     // Create our bracket update with the initial master to be updated
     this.updater = new BracketUpdater({
       sport,
       year,
       currentMaster: master
-    });
+    })
 
     // Helpful for debugging how the watcher was started
-    this.logger.debug('[INIT SCOREWATCHER]', JSON.stringify({master, year, sport}));
+    this.logger.debug('[INIT SCOREWATCHER]', JSON.stringify({master, year, sport}))
 
     // Queue ensures that even if the score watcher emits
     // more than one event, we can still update the master bracket
     // in order, by optionally passing a callback to onSave
-    this.queue = async.queue(this.queueWorker.bind(this), 1);
+    this.queue = async.queue(this.queueWorker.bind(this), 1)
 
     // Drain is mostly just used in tests
-    this.queue.drain = this.options.onDrain;
+    this.queue.drain = this.options.onDrain
   }
 
-  queueWorker(event, cb) {
-    this.logger.debug('[EVENT QUEUED]', JSON.stringify(event));
+  queueWorker (event, cb) {
+    this.logger.debug('[EVENT QUEUED]', JSON.stringify(event))
 
-    const updated = this.updater.update(event);
+    const updated = this.updater.update(event)
 
     if (updated instanceof Error) {
       this.logger.error('[UPDATE FAILED]', JSON.stringify({
         event,
         err: updated.message,
         master: this.updater.currentMaster
-      }));
-      return cb();
+      }))
+      return process.nextTick(cb)
     }
 
-    this.logger.debug('[UPDATED]', updated);
+    this.logger.debug('[UPDATED]', updated)
 
     // If we have a callback, then use that async
     if (this.options.onSave.length === 2) {
-      return this.options.onSave(updated, cb);
+      return this.options.onSave(updated, cb)
     }
 
-    this.options.onSave(updated);
-    return cb();
+    this.options.onSave(updated)
+    return process.nextTick(cb)
   }
 
-  onEvent(event) {
+  onEvent (event) {
     // Team will look like this:
     // { seed: 1, name: ['array of', 'some names'], winner: true }
     const transformTeam = (team) => _.transform(_.pick(team, 'names', 'rank', 'winner'), (res, value, key) => {
       if (key === 'names') {
-        res.name = value;
+        res.name = value
+      } else if (key === 'rank') {
+        res.seed = value
+      } else {
+        res[key] = value
       }
-      else if (key === 'rank') {
-        res.seed = value;
-      }
-      else {
-        res[key] = value;
-      }
-    }, {});
+    }, {})
 
     const result = {
       fromRegion: event.region,
       winner: transformTeam(event.home.winner ? event.home : event.away),
       loser: transformTeam(event.home.winner ? event.away : event.home)
-    };
+    }
 
     if (!result.winner || !result.loser || !result.fromRegion) {
-      this.logger.error('[INVALID RESULT]', JSON.stringify(result));
-    }
-    else {
-      const duplicateTask = _.find(_.map(this.queue.tasks, 'data'), result);
+      this.logger.error('[INVALID RESULT]', JSON.stringify(result))
+    } else {
+      const duplicateTask = _.find(_.map(this.queue.workersList(), 'data'), result)
 
       // Dont allow games that are already in the queue to be added again
       if (duplicateTask) {
-        this.logger.debug('[DUPLICATE UPDATE]', JSON.stringify(result));
-      }
-      else {
-        this.queue.push(result);
+        this.logger.debug('[DUPLICATE UPDATE]', JSON.stringify(result))
+      } else {
+        this.queue.push(result)
       }
     }
   }
 
-  start() {
-    this.scores.start();
-    return this;
+  start () {
+    this.scores.start()
+    return this
   }
 }
 
-module.exports = Watcher;
+module.exports = Watcher
--- test/test.js
+++ test/test.js
@@ -1,38 +1,38 @@
 /* eslint no-magic-numbers:0 */
 
-'use strict';
+'use strict'
 
-const _ = require('lodash');
-const test = require('tape');
-const bracketData = require('bracket-data');
-const Watcher = require('../index');
+const _ = require('lodash')
+const test = require('tape')
+const bracketData = require('bracket-data')
+const Watcher = require('../index')
 
-const year = '2013';
-const sport = 'ncaam';
-const data = bracketData({year, sport});
-const order = data.order;
-const constants = data.constants;
+const year = '2013'
+const sport = 'ncaam'
+const data = bracketData({year, sport})
+const order = data.order
+const constants = data.constants
 
 const scores = {
   url: 'http://mock-url-for-tests.com'
-};
+}
 
 test('It should update the first round all at once', (t) => {
-  const masters = [];
+  const masters = []
 
   const watcher = new Watcher({
     scores,
     year,
     sport,
-    onSave(master) {
-      masters.push(master);
+    onSave (master) {
+      masters.push(master)
     },
-    onDrain() {
-      t.equal(masters.length, 32);
-      t.equal(_.last(masters), 'MW18546372XXXXXXXW18546372XXXXXXXS18546372XXXXXXXE18546372XXXXXXXFFXXX');
-      t.end();
+    onDrain () {
+      t.equal(masters.length, 32)
+      t.equal(_.last(masters), 'MW18546372XXXXXXXW18546372XXXXXXXS18546372XXXXXXXE18546372XXXXXXXFFXXX')
+      t.end()
     }
-  }).scores;
+  }).scores
 
   constants.REGION_IDS.forEach((region) => {
     for (let i = 0, m = order.length; i < m; i += 2) {
@@ -46,14 +46,14 @@ test('It should update the first round all at once', (t) => {
           rank: order[i + 1],
           winner: false
         }
-      });
+      })
     }
-  });
-});
+  })
+})
 
 test('It should update the first round separately if not queued and async', (t) => {
-  let drainCount = 0;
-  const masters = [];
+  let drainCount = 0
+  const masters = []
   const after = [
     'MW1XXXXXXXXXXXXXXWXXXXXXXXXXXXXXXSXXXXXXXXXXXXXXXEXXXXXXXXXXXXXXXFFXXX',
     'MW18XXXXXXXXXXXXXWXXXXXXXXXXXXXXXSXXXXXXXXXXXXXXXEXXXXXXXXXXXXXXXFFXXX',
@@ -63,26 +63,26 @@ test('It should update the first round separately if not queued and async', (t) 
     'MW185463XXXXXXXXXWXXXXXXXXXXXXXXXSXXXXXXXXXXXXXXXEXXXXXXXXXXXXXXXFFXXX',
     'MW1854637XXXXXXXXWXXXXXXXXXXXXXXXSXXXXXXXXXXXXXXXEXXXXXXXXXXXXXXXFFXXX',
     'MW18546372XXXXXXXWXXXXXXXXXXXXXXXSXXXXXXXXXXXXXXXEXXXXXXXXXXXXXXXFFXXX'
-  ];
+  ]
 
   const watcher = new Watcher({
     scores,
     year,
     sport,
-    onSave(master, cb) {
-      masters.push(master);
-      t.equal(master, after[drainCount]);
-      drainCount++;
+    onSave (master, cb) {
+      masters.push(master)
+      t.equal(master, after[drainCount])
+      drainCount++
       if (drainCount === 8) {
-        t.equal(masters.join(','), after.join(','));
+        t.equal(masters.join(','), after.join(','))
       }
-      setTimeout(() => cb(), _.random(100, 150));
+      setTimeout(() => cb(), _.random(100, 150))
     },
-    onDrain() {
-      t.equal(drainCount, 8);
-      t.end();
+    onDrain () {
+      t.equal(drainCount, 8)
+      t.end()
     }
-  }).scores;
+  }).scores
 
   for (let i = 0, m = order.length; i < m; i += 2) {
     watcher.emit('event', {
@@ -95,30 +95,29 @@ test('It should update the first round separately if not queued and async', (t) 
         rank: order[i + 1],
         winner: false
       }
-    });
+    })
   }
-});
+})
 
 test('It should update a few games with gaps', (t) => {
-  let count = 0;
+  let count = 0
 
   const watcher = new Watcher({
     scores,
     year,
     sport,
-    onSave(master) {
+    onSave (master) {
       if (count === 0) {
-        t.equal(master, constants.EMPTY.replace('MWXX', 'MW1X'));
-      }
-      else if (count === 1) {
-        t.equal(master, constants.EMPTY.replace('MWXX', 'MW18'));
+        t.equal(master, constants.EMPTY.replace('MWXX', 'MW1X'))
+      } else if (count === 1) {
+        t.equal(master, constants.EMPTY.replace('MWXX', 'MW18'))
       }
     },
-    onDrain() {
-      count++;
-      if (count === 2) t.end();
+    onDrain () {
+      count++
+      if (count === 2) t.end()
     }
-  }).scores;
+  }).scores
 
   watcher.emit('event', {
     region: 'MW',
@@ -130,7 +129,7 @@ test('It should update a few games with gaps', (t) => {
       rank: 16,
       winner: false
     }
-  });
+  })
 
   setTimeout(() => {
     watcher.emit('event', {
@@ -143,37 +142,6 @@ test('It should update a few games with gaps', (t) => {
         rank: 9,
         winner: false
       }
-    });
-  }, 500);
-});
-
-test('It should only update once if the same game is added to the queue multiple times', (t) => {
-  const masters = [];
-  const watcher = new Watcher({
-    scores,
-    year,
-    sport,
-    onSave(master) {
-      masters.push(master);
-    },
-    onDrain() {
-      t.equal(masters.length, 1);
-      t.equal(masters[0], constants.EMPTY.replace('MWX', 'MW1'));
-      t.end();
-    }
-  }).scores;
-
-  for (let i = 0; i < 5; i++) {
-    watcher.emit('event', {
-      region: 'MW',
-      home: {
-        rank: 1,
-        winner: true
-      },
-      away: {
-        rank: 16,
-        winner: false
-      }
-    });
-  }
-});
+    })
+  }, 500)
+})
